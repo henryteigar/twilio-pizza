322e9caca319bd65b29f6d2ba73839d8fefe85d7
Marko K <mklopets@gmail.com>
2016-04-30 14:54:17 +0300

    Refactoring

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var nlp = require('./nlp');
 var findPizzas = require('./pizza');
 
 
+
 // Twilio Credentials
 var accountSid = process.env.TWILIOSID;
 var authToken = process.env.TWILIOTOKEN;
@@ -25,51 +26,64 @@ var client = require('twilio')(accountSid, authToken);
 
 app.post('/incoming', function(req, res, next) {
 	// Handle incoming SMS messages at this route
-	
+
 	if (!req.body.Body) {
+		// blatantly ignore incorrect requests
 		next();
 	}
-	
-	res.json({message: 'The server successfully read the message and is now working on it'});
 
 	var input = req.body.Body;
-	var inputData = nlp(input.toLowerCase());
-	
-	var results = []; // initialize as empty array
-	
+	var inputData = nlp(input); // parses out data from input string
+
 	console.log(input);
 	console.log(inputData);
-	
+
 	if (inputData) {
-		results = findPizzas(inputData.required, inputData.banned);
-	}
-	
-	// Generate the response SMS
-	if (results && results.length > 0) {
-		var index = Math.floor(Math.random() * results.length);
-		reply = 'You should order: ' + results[index].name + '.';
+		// successfully retrieved parsed information
+		var results = findPizzas(inputData.required, inputData.banned);
+
+		// generate the response sms
+		var reply = 'Sorry, for one reason or another, no suitable pizzas were found.'; // default
+
+		if (results && results.length > 0) {
+			var index = Math.floor(Math.random() * results.length); // randomly generated index for array of criterion-matching pizzas
+			reply = 'You should order: ' + results[index].name + '.';
+		}
+
+		sendSMS(req.body.from, reply);
+		res.json({ message: 'Reply message successfully sent', reply: reply });
 	}
 	else {
-		reply = 'Sorry, for one reason or another, no suitable pizzas were found.';
+		res.status(500).json({message: 'Something went wrong and the server didn\'t get around to sending the message'});
 	}
-	
-	if (reply.length > 0) {
+});
+
+function sendSMS(to, body, from) {
+	// sends text message using Twilio
+
+	from = from || process.env.TWILIO_FROM;
+
+	if (to && body && from) {
 		client.messages.create({
-			to: req.body.From,
-			from: process.env.TWILIO_FROM,
-			body: reply,
+			to: to,
+			from: from,
+			body: body,
 		}, function(err, message) {
 			if (err) {
 				console.log(err);
 			}
 			else {
-				console.log('SMS sent with sid of', message.sid);
+				console.log('sms sent with sid of', message.sid);
 			}
 		});
 	}
-});
+	else {
+		console.log('Message was not sent');
+	}
+}
 
 
 
+// be silent on tests
 console.log('Listening on port', port);
 app.listen(port);
